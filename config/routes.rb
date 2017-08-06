2c7ac4075f8dff88b62fc8548ecaa8fe1d3a9bcb
Ayham Orfali <orfali.ayham@gmail.com>
2017-08-06 16:39:21 +0300

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
+
   root 'static_page#home'
+  get  '/help',    to: 'static_page#help'
+  get  '/about',   to: 'static_page#about'
+  get  '/contact', to: 'static_page#contact'
 
-  get 'static_page/home'
-
-  get 'static_page/help'
-
-  get 'static_page/about'
-
-  get 'static_page/contact'
-
-
+  get '/signup', to: 'user#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

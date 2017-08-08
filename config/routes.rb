d2ad72ccbd3c5211b010ec43e505d9ae6141462f
Ayham Orfali <orfali.ayham@gmail.com>
2017-08-08 14:53:52 +0300

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_page#about'
   get  '/contact', to: 'static_page#contact'
 
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
